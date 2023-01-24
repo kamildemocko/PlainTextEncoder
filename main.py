7f14b55a96cd4715ffff5f238a1472ba9edd0064
Kamil Democko <kamil.democko@gmail.com>
2023-01-25 00:05:53 +0100

    cont developing ux

--- main.py
+++ main.py
@@ -4,18 +4,18 @@ from pathlib import Path
 import flet as ft
 
 from src.engine_handle import predict
-from src.ux_handle import Ux
-
 
 class Main:
     def __init__(self):
         self.page = None
+        self.predicted_encoding_text = None
+        self.file_path = None
 
     def main_flet(self, page: ft.Page):
         self.page = page
 
         def set_predicted_encoding_text(encoding: str):
-            predicted_encoding_text.value = f'Predicted encoding: {encoding}'
+            self.predicted_encoding_text.value = f'Predicted encoding: {encoding}'
             page.update()
 
         def set_predicted_encoding_text_default():
@@ -37,7 +37,7 @@ class Main:
 
             predicted_encoding = predict(path)
 
-            file_path.value = path
+            self.file_path.value = path
             set_predicted_encoding_text(predicted_encoding)
 
         def on_file_picker_dialog(e: ft.FilePickerResultEvent):
@@ -61,45 +61,60 @@ class Main:
             if subtitle_file_path:
                 handle_filepath_inserted(subtitle_file_path)
 
-        page.window_width = 800
-        page.window_height = 300
+        def build_app():
+            page.window_width = 800
+            page.window_height = 300
 
-        # title
-        title = ft.Text('Subtitle Encoder', size=30)
-        title_container = ft.Container(content=title, alignment=ft.alignment.center)
+            title = ft.Text('Subtitle Encoder', size=30)
+            title_container = ft.Container(content=title, alignment=ft.alignment.center)
 
-        file_picker = ft.FilePicker(on_result=on_file_picker_dialog)
-        page.overlay.append(file_picker)
+            file_picker = ft.FilePicker(on_result=on_file_picker_dialog)
+            page.overlay.append(file_picker)
 
-        file_path = ft.TextField(
-            label='Insert file path OR choose subtitle file through file explorer',
-            expand=True,
-            text_size=13,
-            on_change=on_path_value_change,
-        )
+            self.file_path = ft.TextField(
+                label='Insert file path OR choose subtitle file through file explorer',
+                width=600,
+                text_size=13,
+                on_change=on_path_value_change,
+            )
 
-        file_btn = ft.ElevatedButton(
-            'Choose file',
-            on_click=lambda _: file_picker.pick_files(allowed_extensions=['srt', 'sub', 'ass'])
-        )
-        fu_row = ft.Row(controls=[
-            file_path,
-            file_btn
-        ])
-        fu_container = ft.Container(content=fu_row, margin=ft.margin.symmetric(0, 20))
+            file_btn = ft.ElevatedButton(
+                'Choose file',
+                on_click=lambda _: file_picker.pick_files(allowed_extensions=['srt', 'sub', 'ass'])
+            )
+            fu_row = ft.Row(controls=[
+                self.file_path,
+                file_btn
+            ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN)
+            fu_container = ft.Container(content=fu_row, margin=ft.margin.symmetric(0, 20))
 
-        predicted_encoding_text = ft.Text(size=16)
-        set_predicted_encoding_text_default()
-        predicted_encoding_container = ft.Container(content=predicted_encoding_text, expand=True, margin=ft.margin.symmetric(0, 20))
+            self.predicted_encoding_text = ft.Text(size=16)
+            set_predicted_encoding_text('NO FILE CHOSEN')
+            self.predicted_encoding_hint = ft.Text('AA', size=16)
+            predicted_row = ft.Row(controls=[
+                self.predicted_encoding_text,
+                self.predicted_encoding_hint
+            ], vertical_alignment=ft.CrossAxisAlignment.START)
+            predicted_encoding_container = ft.Container(
+                content=predicted_row,
+                margin=ft.margin.symmetric(0, 20),
+                expand=True,
+            )
 
-        page.add(
-            title_container,
-            fu_container,
-            predicted_encoding_container
-        )
+            page.add(
+                title_container,
+                fu_container,
+                predicted_encoding_container
+            )
+
+        build_app()
 
     def start(self):
         ft.app(target=self.main_flet)
 
 
-Main().start()
+if __name__ == '__main__':
+    main = Main()
+    main.start()
+
+
