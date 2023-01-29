f27b618356292e20aa819010d3eca605b1785658
Kamil Democko <kamil.democko@gmail.com>
2023-01-29 11:08:44 +0100

    added simple thread for conversion

--- src/engine_handle.py
+++ src/engine_handle.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from shutil import copy2
+import threading
 
 import chardet
 
@@ -16,27 +17,30 @@ def predict(path: Path, accuracy: int = 100) -> str:
 
 
 def convert_file(path: Path, encoding_in: str, encoding_out: str):
-    print(encoding_in)
-    print(encoding_out)
+    def _convert_file():
+        try:
+            with backup_file.open(mode='rb') as fr, \
+                    path.open(mode='wb') as fw:
+                while True:
+                    try:
+                        txt = next(fr).decode(encoding_in, errors='ignore')
+                        fw.write(txt.encode(encoding_out, errors='ignore'))
+                    except StopIteration:
+                        break
+
+            backup_file.unlink()
+
+        except UnicodeEncodeError:
+            path.unlink()
+            backup_file.rename(path)
+            raise Exception('Unicode Encoding Error')
+
     if not path.exists():
         raise FileNotFoundError
 
     backup_file = Path(f'{path}.bak')
     copy2(path, backup_file)
 
-    try:
-        with backup_file.open(mode='rb') as fr, \
-                path.open(mode='wb') as fw:
-            while True:
-                try:
-                    txt = next(fr).decode(encoding_in, errors='ignore')
-                    fw.write(txt.encode(encoding_out, errors='ignore'))
-                except StopIteration:
-                    break
-
-        backup_file.unlink()
-
-    except UnicodeEncodeError:
-        path.unlink()
-        backup_file.rename(path)
-        raise Exception('Unicode Encoding Error')
+    thread = threading.Thread(target=_convert_file)
+    thread.start()
+    thread.join()
